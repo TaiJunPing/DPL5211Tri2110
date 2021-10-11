319513435a74fe8b2cae5bc905ce25757ab50c9a
TaiJunPing <90165779+TaiJunPing@users.noreply.github.com>
2021-10-11 10:31:56 +0800

    Add files via upload

---
+++ Lab 5.3 .py
@@ -0,0 +1,42 @@
+# Student ID : 1201201025
+# Student Name : Tai Jun Ping
+
+# Display the menu
+# Ask the user to enter their choice [1 or 2]
+# Using a switch-case statement
+# If choice is 1 call function get_cm()
+# If choice is 2 call function get_meter()
+#    Else print "Invalid choice"
+# In get_cm():
+# Get the value of centimeter from the User
+# Call function cm_to_meter(...) and pass centimeter to calculate meter
+# Display the centimeter and meter
+
+print("====================================")
+print("\t       MENU")
+choice = (input("Enter your choice [1|2] :"))
+
+def get_cm():
+    cm = float(input("Please enter a value for centimeter : "))
+    m = cm_to_meter(cm)
+    print(" {:.2f} cm is {:.2f} meter".format(cm,m))
+
+def cm_to_meter(centimeter):
+    meter = centimeter / 100
+    return meter
+
+def get_meter():
+    m = float(input("Please enter a value for meter : "))
+    cm = meter_to_cm(m)
+    print(" {:.2f} meter is {:.2f} cm".format(m,cm))
+
+def meter_to_cm(meter):
+    centimeter = meter * 100
+    return centimeter
+
+if(choice=='1'):
+    get_cm()
+elif(choice=='2'):
+    get_meter()
+else:
+    print("INVALID CHOICE")
